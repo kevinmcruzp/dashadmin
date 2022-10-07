2780adabd7cb4d3f30af412ea9c6916654a2f71c
Kevin <kevin_mcp@outlook.com>
2022-10-07 18:03:19 -0300

    Pagina de users, onde se criou uma tabela com a listagem de usuarios

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,135 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuarios
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              bg="pink.500"
+              leftIcon={<Icon as={RiAddLine} />}
+              _hover={{ opacity: 0.9 }}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Kevin Cruz</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      kevin@email.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td> 07/10/2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="17" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Kevin Cruz</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      kevin@email.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td> 07/10/2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="17" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Kevin Cruz</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      kevin@email.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td> 07/10/2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="17" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
