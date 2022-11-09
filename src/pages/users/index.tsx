446bb0f9878fc9d97e7fd199a1b5a42470e36155
Kevin <kevin_mcp@outlook.com>
2022-11-09 13:21:07 -0300

    refactor: sinalizando se esta buscando dados com isfetching

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,7 +23,7 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error, refetch } = useQuery(
     ["users"],
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -65,6 +65,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuarios
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
